f92f236980a340246da21a94c3e81e3fe8de9fdf
Torge <torge.rau@gmx.de>
2024-03-09 02:17:41 +0100

    Header bearbeitet und fast fertig

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 import './Header.css'
+import Startseitenbild from '../../assets/Startseite.jpeg'
 
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className = "tus__header section__padding" id = "home">
+      <div className = "tus__header-content">
+        <p className = "welcome__text">Willkommen beim</p>
+        <h1 className = "gradient__text">TuS Kaisersesch</h1>
+        <p>Dem Sportverein aus der Vordereifel. <br />
+           Auf der Suche nach einem Sportverein?<br />
+           Egal ob erfahrener Sportler oder Anfänger, <br />
+           Egal ob jung oder alt, <br />
+           Bei uns findest Du genau das richtige für dich!
+        </p>
+        <div className = "tus__header-content-buttons">
+          <button type = "button"><a href = "MehrErfahren">Erfahre Mehr</a></button>
+          <button type = "button"><a href = "#Mitgliedwerden">Mitglied werden</a></button>
+        </div>
+        <div className = "tus__header-image">
+          <img src = {Startseitenbild} alt = "Startseite"/>
+        </div>
+      </div>
+      
     </div>
   )
 }
